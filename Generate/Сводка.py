396a20a1d00de0d1f8f82b37f01efe9ead619a54
ShavedHedgehoc <48063136+ShavedHedgehoc@users.noreply.github.com>
2020-03-06 19:33:56 +0300

    1321313

--- Generate/Сводка.py
+++ Generate/Сводка.py
@@ -14,9 +14,17 @@ if __name__ == '__main__':
         df = pd.read_excel(xl, sheet_name=sh, header=None, dtype=str)
         cur_date = df.iloc[0, 14]
         df2 = df.dropna(subset=[8, 16])
+        df_collect = None
         for index, row in df2.iterrows():
             if row[1] == "набор":
                 set_batch = row[4]
             if pd.isna(row[0]):
-                print("Набор %s, %s, %s, %s" %
-                      (set_batch, row[2], row[3], row[4]))
+                pass
+                # print("Набор %s, %s, %s, %s" %
+                #      (set_batch, row[2], row[3], row[4]))
+            else:
+                a = pd.DataFrame([[cur_date, row[0], row[3], row[4], row[8]]],                                
+                                 columns=['Дата','Код', 'Артикул', 'Партия', 'Конвейер'])
+                df_collect = pd.concat(
+                    [df_collect, a], ignore_index=True)
+        df_collect.to_excel('Сводка.xlsx', index=False, header=True)
